f165a2befccdd3e411575dcd70d0ea359471e4bf
solankimanish111-web <solankimanish111@gmail.com>
2026-04-11 13:58:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ function generateImage(){
 
 let prompt = document.getElementById("prompt").value;
 
-let imageURL = "https://source.unsplash.com/600x600/?" + prompt;
+if(prompt===""){
+alert("Please type something");
+return;
+}
 
-document.getElementById("output").src = imageURL;
+let url = "https://source.unsplash.com/600x600/?" + prompt;
+
+document.getElementById("output").src = url;
 
 }
